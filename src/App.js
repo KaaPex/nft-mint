184193246ab18c429f5c5d155345d3090e00c8f6
Aleksey Kazantsev <develop.kaa@gmail.com>
2022-05-16 13:48:00 +0300

    add some refactoring add connect wallet button

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, {useEffect,useState} from 'react';
+import './App.css';
+
+const App = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana && solana.isPhantom) {
+        console.log('Phantom wallet found!');
+
+        const response = await solana.connect({ onlyIfTrusted: true });
+
+        setWalletAddress(response.publicKey.toString());
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Candy Drop</p>
+
+          <p className="sub-text">NFT drop machine with fair mint</p>
+
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+
+        <div className="footer-container">
+          Abrakadabra
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
